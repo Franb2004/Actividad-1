6c3594beaf501459c66b33fe5d46212218ecacbf
Franb2004 <111138292+Franb2004@users.noreply.github.com>
2022-08-30 09:37:04 -0300

    Add files via upload

---
+++ Actividad 1/Actividad 1/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If Application.OpenForms.Count > 1 Then
+            Hide()
+        Else
+            Application.Exit()
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.Show()
+        Dim parar As Integer
+        Do While parar < 1000000
+
+            Me.Label1.Text = parar.ToString()
+
+            parar = parar + 1
+
+        Loop
+    End Sub
+
+    Private Sub Form1_Activated(sender As Object, e As EventArgs) Handles MyBase.Activated
+        Form2.Show()
+    End Sub
+End Class
